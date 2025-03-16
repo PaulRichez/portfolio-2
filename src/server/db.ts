12146d0b2427f62af8fc633761f7bfa1b021c9d2
paul.richez <paul.richez@rewayz.com>
2025-03-16 16:50:53 +0100

    feat: reset

--- src/server/db.ts
+++
@@ -1,7 +0,0 @@
-import knex from "knex";
-
-// Utilisation de require pour éviter le problème de typage
-const config = require("../../knexfile");
-
-const environment = "production";
-export const db = knex(config[environment]);
--- src/server/routes/experiences.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { ExperiencesController } from '../controllers/experiences.controller';
-
-const router = Router();
-
-router.get('/', ExperiencesController.getExperiences);
-
-export default router;
