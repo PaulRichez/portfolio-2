1640119a18db50490ccd8735252362667fa09508
paul.richez <paul.richez@rewayz.com>
2025-05-29 20:37:04 +0200

    feat

--- portfolio-client/src/app/pages/home/sections/me/me.component.ts
+++ portfolio-client/src/app/pages/home/sections/me/me.component.ts
@@ -1,12 +1,25 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { ChipModule } from 'primeng/chip';
+import { DividerModule } from 'primeng/divider';
 import { Me } from '../../../../models';
 
 @Component({
   selector: 'app-me',
-  imports: [],
+  imports: [CommonModule, CardModule, AvatarModule, ButtonModule, ChipModule, DividerModule],
   templateUrl: './me.component.html',
   styleUrl: './me.component.scss'
 })
 export class MeComponent {
   @Input() me: Me | null = null;
+
+  getInitials(): string {
+    if (!this.me?.firstName || !this.me?.lastName) {
+      return 'NA';
+    }
+    return this.me.firstName.charAt(0) + this.me.lastName.charAt(0);
+  }
 }
