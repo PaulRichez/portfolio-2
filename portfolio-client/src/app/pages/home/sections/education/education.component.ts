c99982f4c99b5b3d5790eb7702d997aea364b505
paul.richez <paul.richez@rewayz.com>
2025-05-30 14:08:27 +0200

    feat

--- portfolio-client/src/app/pages/home/sections/education/education.component.ts
+++ portfolio-client/src/app/pages/home/sections/education/education.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimelineModule } from 'primeng/timeline';
 import { CardModule } from 'primeng/card';
@@ -12,6 +12,24 @@ import { Diploma } from '../../../../models';
   templateUrl: './education.component.html',
   styleUrl: './education.component.scss'
 })
-export class EducationComponent {
+export class EducationComponent implements OnInit {
   @Input() diplomas: Diploma[] = [];
+  timelineAlign: 'left' | 'right' | 'alternate' = 'alternate';
+
+  ngOnInit() {
+    this.updateTimelineAlign();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.updateTimelineAlign();
+  }
+
+  private updateTimelineAlign() {
+    if (window.innerWidth <= 768) {
+      this.timelineAlign = 'left';
+    } else {
+      this.timelineAlign = 'alternate';
+    }
+  }
 }
