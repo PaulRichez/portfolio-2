725eac807a904e02ed0ca4be8d7b854592339f34
paul.richez <paul.richez@rewayz.com>
2025-06-09 14:09:39 +0200

    feat

--- portfolio-server/config/middlewares.ts
+++ portfolio-server/config/middlewares.ts
@@ -2,14 +2,7 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: ['http://localhost:4201', 'http://localhost:3000'],
-    }
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
