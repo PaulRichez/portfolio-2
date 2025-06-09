eb1a656203829b9c5dd9d3119785cb2359e48c1d
paul.richez <paul.richez@rewayz.com>
2025-06-09 15:33:03 +0200

    feat

--- portfolio-server/config/middlewares.ts
+++ portfolio-server/config/middlewares.ts
@@ -2,7 +2,15 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: ['*'],
+      origin: ['http://localhost:4201', 'http://localhost:3000'], // Add your Angular app URLs
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
